f8a60d9f6453dab709df40e9c57509b847690b22
Alejandro Luna <alejandro2002.uar@gmail.com>
2024-08-02 20:51:12 -0300

    Added style in view login

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -5,16 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <form action="{{ route('authenticate') }}" method="POST">
-        @csrf
-        <h1>Sing in</h1>
-        <label for="email">Email</label><br>
-        <input type="email" name="email" required><br>
-        <label for="password">Password</label><br>
-        <input type="password" name="password" required><br>
-        <button type="submit">Login</button>
-    </form>
+    <div class="d-flex justify-content-center align-items-center" style="height: 100vh">
+        <form action="{{ route('authenticate') }}" method="POST">
+            @csrf
+            <h3 class="text-center">Sing in</h3>
+            <div class="mb-3">
+              <label for="email" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="email" name="email">
+            </div>
+            <div class="mb-3">
+              <label for="password" class="form-label">Password</label>
+              <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <div class="d-flex justify-content-center">
+                <button type="submit" class="btn btn-primary w-100 p-2">Login</button>
+            </div>
+          </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
