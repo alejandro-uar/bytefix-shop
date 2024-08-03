680e4e32822b9a11bc7746bec99a8913f4c265eb
Alejandro Luna <alejandro2002.uar@gmail.com>
2024-08-02 21:06:50 -0300

    Added style in view register

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -5,18 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Register</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<body>
-    <form action="{{ route('register') }}" method="POST">
-        @csrf
-        <h1>Register</h1>
-        <label for="name">Full name</label><br>
-        <input type="text" name="name" required><br>
-        <label for="email">Email</label><br>
-        <input type="email" name="email" required><br>
-        <label for="password">Password</label><br>
-        <input type="password" name="password" required><br>
-        <button type="submit">Register</button>
-    </form>
+<body style="background: #04b">
+    <div class="d-flex justify-content-center align-items-center" style="height: 100vh">
+        <form action="{{ route('register') }}" method="POST" class="p-4 rounded bg-white">
+            @csrf
+            <h3 class="">Register user</h3>
+            <div class="mb-3">
+                <label for="name" class="form-label">Full name</label>
+                <input type="name" class="form-control" id="name" name="name">
+              </div>
+            <div class="mb-3">
+              <label for="email" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="email" name="email">
+            </div>
+            <div class="mb-3">
+              <label for="password" class="form-label">Password</label>
+              <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <div class="d-flex justify-content-center">
+                <button type="submit" class="btn btn-primary w-100 p-2">Register</button>
+            </div>
+          </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
