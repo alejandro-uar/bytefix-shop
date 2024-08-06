f23dd57ecff4ff92592ed8f66cc1dfff44b65219
Alejandro Luna <alejandro2002.uar@gmail.com>
2024-08-06 17:07:47 -0300

    Add btn-logout in navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,9 @@
+@php
+  $value = true;    
+@endphp
+
 <nav class="navbar navbar-expand-lg" style="background:#6610f2;">
-    <div class="container-fluid">
+    <div class="container-fluid">      
       <a class="navbar-brand badge text-bg-dark p-2 " href="{{route('index')}}" style="font-size:1.2em">ByteFix product list</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -12,6 +16,12 @@
             </li>
             <li class="nav-item">
               <a class="nav-link text-light" href="{{route('product')}}">+New Product</a>
+            </li>
+            <li class="nav-item">
+              <form action="{{route('logout')}}" method="POST">
+                  @csrf
+                  <button type="submit" class="btn btn-light ">Logout</button>
+              </form>
             </li>
           </ul>
         </div>
